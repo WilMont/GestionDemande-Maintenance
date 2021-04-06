0547fe6471c55ab328a65115f6213971f83ca168
hamit1994 <mhamitissaka@gmail.com>
2021-04-06 10:05:50 +0200

    Ajout Saisie TTC et TVA

---
+++ Saisie_TTC_TVA.js
@@ -0,0 +1,8 @@
+
+function Saisie_TCC_TVA(){
+
+    var TCC=prompt("Entrer le montant de TCC :")
+    var TVA=prompt("Entrer le montant de TVA :")
+}
+
+module.exports={Saisie_TCC_TVA}
--- app.js
+++ app.js
@@ -1,7 +1,1 @@
 console.log("Hello World");
-
-function Saisie_TCC_TVA(){
-
-    var TCC=prompt("Entrer le montant de TCC :")
-    var TVA=prompt("Entrer le montant de TVA :")
-}
