4fe23acf51f146f5587253bba8219c183ad574c0
Wilfried MONTAGNE <wilfried.montagne@gmail.com>
2021-04-06 09:49:27 +0200

    Ajout du tableau de correspondance Pays/TVA

---
+++ TableauPaysTVA.js
@@ -0,0 +1,15 @@
+function afficherTableauPaysTVA() {
+    console.log(
+        "╔═══════════╦═══════╗\n" +
+        "║ Code Pays ║ % TVA ║\n" +
+        "╠═══════════╬═══════╣\n" +
+        "║    FR     ║  20   ║\n" +
+        "║    EN     ║  20   ║\n" +
+        "║    DE     ║  16   ║\n" +
+        "║    ES     ║  21   ║\n" +
+        "║    IT     ║  22   ║\n" +
+        "║    PR     ║  23   ║\n" +
+        "╚═══════════╩═══════╝"
+    );
+}
+module.exports = {afficherTableauPaysTVA};
---
+++ app.js
@@ -0,0 +1,3 @@
+const TableauPaysTVA = require('./TableauPaysTVA');
+
+TableauPaysTVA.afficherTableauPaysTVA();
